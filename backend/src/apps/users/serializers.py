603f11148d8e42e68e871a3f0ed9a0c21c2bc2b6
Ali Akbari <7731.aa@gmail.com>
2025-10-17 21:43:39 +0000

    implement login, verify otp views

--- backend/src/apps/users/serializers.py
+++ backend/src/apps/users/serializers.py
@@ -1,18 +1,26 @@
+from django.contrib.auth import get_user_model
 from rest_framework import serializers
 
+User = get_user_model()
 
-class RegisterSerializer(serializers.Serializer):
-    username = serializers.CharField(required=True)
-    email = serializers.CharField(required=True)
-    password1 = serializers.CharField(required=True)
-    password2 = serializers.CharField(required=True)
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, required=True, style={'input_type': 'password'})
+    password2 = serializers.CharField(write_only=True, required=True, style={'input_type': 'password'})
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password', 'password2']
 
     def validate(self, attrs):
-        password1 = attrs.get('password1')
-        password2 = attrs.get('password2')
-        if password1 != password2:
-            raise serializers.ValidationError('Password do not match.')
+        if attrs.get('password') != attrs.get('password2'):
+            raise serializers.ValidationError({'password': 'Password do not match.'})
         return attrs
+    
+    def create(self, validated_data):
+        validated_data.pop('password2')
+        user = User.objects.create_user(**validated_data)
+        return user
 
 
 class LoginSerializer(serializers.Serializer):
