e1826bbce591706d849dac957ac0ac8fdaa78b01
Mehrshad Valizadeh Arjmand <mehrshad03@yandex.com>
2025-10-18 16:23:25 +0330

    Add STATIC_ROOT to settings.py

--- backend/src/config/settings.py
+++ backend/src/config/settings.py
@@ -165,6 +165,8 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 
+STATIC_ROOT = BASE_DIR / "static files"
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
