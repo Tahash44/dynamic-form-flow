09e1ac1cb7f4ac34a0bf5a0552af165d960fbac2
MohammadHosein <mhfathali2003@gmail.com>
2025-10-20 18:30:20 +0330

    add django-filter dependency and register it in INSTALLED_APPS

--- backend/src/config/settings.py
+++ backend/src/config/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "rest_framework",
     "drf_yasg",
+    'django_filters',
     'rest_framework_swagger',
     "config.api",
     "apps.users",
