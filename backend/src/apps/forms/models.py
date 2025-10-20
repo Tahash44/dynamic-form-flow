93d1cc12514bce41afbd62a8f79e5b1a2ab2fe0c
MohammadHosein <mhfathali2003@gmail.com>
2025-10-20 11:13:03 +0330

    Add date and number models and a few small fixes

--- backend/src/apps/forms/models.py
+++ backend/src/apps/forms/models.py
@@ -2,6 +2,8 @@ from config.settings import AUTH_USER_MODEL
 from django.core.exceptions import ValidationError
 from django.db import models
 from django.core.validators import MinValueValidator
+from django.core.exceptions import ValidationError
+from django.utils import timezone
 
 class Form(models.Model):
     title = models.CharField(max_length=255)
@@ -28,7 +30,7 @@ class FinalPage(models.Model):
 
 
 class Field(models.Model):
-    form = models.ForeignKey('Form', on_delete=models.CASCADE)
+    form = models.ForeignKey('Form', on_delete=models.CASCADE,related_name='fields')
     position = models.PositiveSmallIntegerField(default=0)
     question = models.CharField(max_length=255)
     description = models.TextField(null=True, blank=True)
@@ -50,7 +52,7 @@ def validate_string_list(value):
             raise ValidationError('All items must be strings')
 
 
-class CheckBoxField(Field):
+class FormCheckBoxField(Field):
     check_box_names = models.JSONField(
         default=list,
         validators=[validate_string_list],
@@ -62,14 +64,36 @@ class CheckBoxField(Field):
     )
 
 
-class SelectField(Field):
+class FormSelectField(Field):
     options = models.JSONField(
         default=list,
         validators=[validate_string_list],
     )
 
 
-class TextField(Field):
+class FormTextField(Field):
     max_length = models.PositiveSmallIntegerField(
         validators=[MinValueValidator(1)],
     )
+
+class FormNumberField(Field):
+    min_value = models.FloatField(null=True, blank=True)
+    max_value = models.FloatField(null=True, blank=True)
+    decimal_allowed = models.BooleanField(default=False)
+
+    def clean(self):
+        super().clean()
+        if self.min_value is not None and self.max_value is not None:
+            if self.min_value > self.max_value:
+                raise ValidationError("min_value cannot be greater than max_value.")
+
+class FormDateField(Field):
+    min_date = models.DateTimeField(null=True, blank=True)
+    max_date = models.DateTimeField(null=True, blank=True)
+    include_time = models.BooleanField(default=False)
+
+    def clean(self):
+        super().clean()
+        if self.min_date and self.max_date and self.min_date > self.max_date:
+            raise ValidationError("min_date cannot be greater than max_date.")
+
